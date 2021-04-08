fe8ee851596950cfeca150c2d103cd92c3ad8d3c
B.M <bigman1208000@gmail.com>
2021-04-08 15:28:34 +0300

    installed styled-system styled-components & created Box, Button and Text

--- components/NavigationButton/index.js
+++ components/NavigationButton/index.js
@@ -3,8 +3,18 @@ import React from 'react';
 //styles
 import styles from './index.module.css';
 
+//components - styled-system
+import Box from '../StyledSystem/box';
+import Text from '../StyledSystem/text';
+import Button from '../StyledSystem/button';
+
 function NavigationButton() {
-  return <div className={styles.div} />;
+  return (
+    <Box className={styles.div}>
+      <Text>TEST</Text>
+      <Button>TEST</Button>
+    </Box>
+  );
 }
 
 export default NavigationButton;
